0560e0a1bfe9e5bbf10d8e0eed869d644b8d6c5f
nth.dlnq2909@gmail.com <nth.dlnq2909@gmail.com>
2020-11-08 13:17:56 +0700

    Add Entity

---
+++ eShopSolution.Data/EF/EShopDBContext.cs
@@ -0,0 +1,18 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    class EShopDBContext : DbContext
+    {
+        public EShopDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
